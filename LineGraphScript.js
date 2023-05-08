fbebb780994538a6eaa7ca8a19986534c9ab9e4b
KyleGibbs <102107602@student.swin.edu.au>
2023-05-08 10:51:38 +1000

    Fixed Inflation Graph

--- LineGraphScript.js
+++ LineGraphScript.js
@@ -1,41 +1,17 @@
-lineInit() {
+function lineInit() {
     w = 600;
     h = 300;
     padding = 20;
 
-    var dataset;
+    var dset;
 
-    d3.csv("InflationRate.csv", function(data) {
-        var countries;
-        var dates;
-
-        for (var variable in data[0]) {
-            if (variable != ""){
-                if (key == "Country"){
-                    countries = data.map(function(d) { return d[variable]; });         
-                    //loops through everything in the country column and adds it to the array. Map is used to extract the data
-                }
-                } else {
-                    dates.push(variable);
-                }
-        }
-
-        var values = [];
-
-        data.forEach(function(d) {                             //Loop through every element in data, creating an object for each line
-            var country = d.Country;
-            for (var i = 0; i < dates.length; i++) {
-                var date = dates[i];
-                var value = +d[date];
-                values.push({
-                    country: country,
-                    date: date,
-                    value: value
-                });
-            }
-        });
-
-
-
+    d3.csv("Files/InflationRate.csv", function(d) {
+        return {
+            country: d.Country,
+            dateValues: [+d.Jan18, +d.Apr18, +d.Oct18, +d.Jan19, +d.Apr19, +d.Jul19, +d.Oct19, +d.Jan20, +d.Apr20, +d.Jul20, +d.Oct20,  +d.Jan21, +d.Apr21, +d.Jul21, +d.Oct21, +d.Jan22, +d.Apr22, +d.Jul22, +d.Oct22]
+        };   
+    }).then(function(data) {
+        dataset = data;
+        console.table(dataset, ["country", "dateValues"]);
     });
 }
