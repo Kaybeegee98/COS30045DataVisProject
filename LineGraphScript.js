00b47eeb4fdb3311acafe25e4357d980e2a47a9a
KyleGibbs <102107602@student.swin.edu.au>
2023-05-10 23:14:57 +1000

    Inflation Rate Base Done

--- LineGraphScript.js
+++ LineGraphScript.js
@@ -3,15 +3,174 @@ function lineInit() {
     h = 300;
     padding = 20;
 
-    var dset;
+    var dataset;
 
-    d3.csv("Files/InflationRate.csv", function(d) {
+    d3.csv("Files/InflationRateSecondAttempt.csv", function(d) {
         return {
-            country: d.Country,
-            dateValues: [+d.Jan18, +d.Apr18, +d.Oct18, +d.Jan19, +d.Apr19, +d.Jul19, +d.Oct19, +d.Jan20, +d.Apr20, +d.Jul20, +d.Oct20,  +d.Jan21, +d.Apr21, +d.Jul21, +d.Oct21, +d.Jan22, +d.Apr22, +d.Jul22, +d.Oct22]
-        };   
+            date: d.Date,
+            AustraliaValues: +d.AustraliaValues,
+            EnglandValues: +d.EnglandValues,
+            IndiaValues: +d.IndiaValues,
+            ChinaValues: +d.ChinaValues,
+            NewZealandValues: +d.NewZealandValues,
+            PhilippinesValues: +d.PhilippinesValues,
+            SouthAfricaValues: +d.SouthAfricaValues
+        };
     }).then(function(data) {
         dataset = data;
         console.table(dataset, ["country", "dateValues"]);
+
+        lineChart(dataset, w, h, padding);
     });
+}
+
+function lineChart(data, w, h, padding) {
+    console.log(data);
+
+    var parseTime = d3.timeParse("%b-%y");
+    var dates = [];
+    for(let obj of data) {
+        dates.push(parseTime(obj.date));
+    }
+
+    xScale = d3.scaleTime()
+                .domain(d3.extent(dates))
+                .range([0, w]);
+
+    yScale = d3.scaleLinear()
+                .domain([0, d3.max(data, function (d) { return d.AustraliaValues; })
+                ])
+                .range([h, 0]);
+
+    australialine = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.AustraliaValues)});
+
+    englandline = d3.line()
+                    .x(function(d) { return xScale(parseTime(d.date)); })
+                    .y(function(d) { return yScale(d.EnglandValues)});
+
+    var svg = d3.select("#lineGraph")
+                .append("svg")
+                .attr("width", w + 2 * padding)
+                .attr("height", h + 2 * padding);
+
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("transform", "translate(20, 0)")
+        .attr("d", australialine)
+        .attr("stroke", "blue");
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "secondline")
+        .attr("transform", "translate(20, 0)")
+        .attr("d", englandline)
+        .attr("stroke", "red");
+
+    var xAxis = d3.axisBottom()
+                    .scale(xScale);
+
+    svg.append("g")
+        .attr("transform", "translate(20, " + (h + (padding/2)) + ")")
+        .call(xAxis);
+
+    var yAxis = d3.axisLeft()
+        .ticks(10)
+        .scale(yScale); 
+
+    svg.append("g")
+        .attr("transform", "translate(20 , 10)")                        //Position Y-Axis Correctly
+        .call(yAxis);
+}
+
+function lineSelectGraph() {
+    var choice = document.getElementById("lineSelect").value;
+
+    update(choice);
+}
+
+function update(choice){
+    var dataset;
+
+    d3.csv("Files/InflationRateSecondAttempt.csv", function(d) {
+        return {
+            date: d.Date,
+            AustraliaValues: +d.AustraliaValues,
+            EnglandValues: +d.EnglandValues,
+            IndiaValues: +d.IndiaValues,
+            ChinaValues: +d.ChinaValues,
+            NewZealandValues: +d.NewZealandValues,
+            PhilippinesValues: +d.PhilippinesValues,
+            SouthAfricaValues: +d.SouthAfricaValues
+        };
+    }).then(function (data){
+        dataset = data;
+    })
+
+    var parseTime = d3.timeParse("%b-%y");
+
+    englandline = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.EnglandValues)});
+
+    indialine = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.IndiaValues)});
+    
+    chinaline = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.ChinaValues)});
+        
+    newzealandline = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.NewZealandValues)});
+        
+    philippinesline = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.PhilippinesValues)});
+        
+    southafricaline = d3.line()
+                .x(function(d) { return xScale(parseTime(d.date)); })
+                .y(function(d) { return yScale(d.SouthAfricaValues)});
+
+    var secondline;
+    switch(choice) {
+        case "England":
+            secondline = englandline;
+            break;
+
+        case "India":
+            secondline = indialine;
+            break;
+        
+        case "China":
+            secondline = chinaline;
+            break;
+
+        case "NewZealand":
+            secondline = newzealandline;
+            break;
+        
+        case "Philippines":
+            secondline = philippinesline;
+            break;
+
+        case "SouthAfrica":
+            secondline = southafricaline;
+            break;
+
+        default:
+            secondline = australialine;
+            break;
+    }
+
+    var svg = d3.select("#lineGraph")
+                .transition();
+
+    svg.select(".secondline")
+        .duration(750)
+        .attr("d", secondline);
 }
