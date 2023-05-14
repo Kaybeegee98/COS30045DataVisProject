16e8c510c720579f80eced9e539286a02e939e11
lachieho88 <103191842@student.swin.edu.au>
2023-05-14 20:25:18 +1000

    updated the bar chart

--- BarGraphScript.js
+++ BarGraphScript.js
@@ -1,5 +1,6 @@
 //This is for Lachie's Bar Graph Script
-function init() {
+
+function barInit() {
 
     d3.csv("/Files/NetMigration.csv").then(function (data) {
         console.log(data);
@@ -9,12 +10,16 @@ function init() {
 
     function barChart() {
         var w = 500; // Width
-        var h = 100; //height
+        var h = 500; //height
         var barpadding = 4;
-        var svg = d3.select(".barGraph")
+        var svg = d3.select("#barGraph")
             .append("svg")
             .attr("width", w)
-            .attr("height", h);
+            .attr("height", h)
+            .append("g")
+            .attr("transform", "translate(20, 0)"); // Shift the chart to the right by 50 pixels
+        
+            
         //selection.attr(name[, value]);
 
         //Create the x-scale
@@ -25,15 +30,16 @@ function init() {
         //Create the y-scale
         var yScale = d3.scaleLinear()
             //  .domain([0, d3.max(migrationData)])
-            .domain([0, d3.max(migrationData, d => d.Net_Overseas_Migration_Thousands)])
+            .domain([ 0,d3.max(migrationData, d => d.Net_Overseas_Migration_Thousands) ])
             .range([0, h]);
         //Create x-axis
         var xAxis = d3.axisBottom()
             .scale(xScale);
         svg.append("g")
-            .attr("transform", "translate(0, " + (h - barpadding) + ")")
+            .attr("transform", "translate(0, " + (h - 20) + ")")
             .call(xAxis);
 
+        //Create the y-axis
         var yAxis = d3.axisLeft()
             .scale(yScale);
 
@@ -51,37 +57,27 @@ function init() {
             .data(migrationData)
             .enter()
             .append("rect")
-            /*
             .attr("x", function (d, i) {
                 return i * (w / migrationData.length);
             })
-            */
+          
            
             .attr("x", function (d) {
                 return xScale(d.Year);
             })
             
-            /*
-             .attr("y", function (d) {
-                 return h - (d.Net_Overseas_Migration_Thousands / 3);
-             })
-             */
             .attr("y", function (d) {
                 return h - yScale(d.Net_Overseas_Migration_Thousands);
 
             })
-            /*
-             .attr("height", function (d) {
-                 return d.Net_Overseas_Migration_Thousands / 3;
-             })
-             */
+             
+           
             .attr("height", function (d) {
                 return yScale(d.Net_Overseas_Migration_Thousands - 13)
             })
-
-            /*
-            .attr("width", w / migrationData.length - 5)
-            */
+  
+            
+            
             .attr("width", w / migrationData.length - 5)
             .attr("fill", function (d) {
                 return "rgb(0,0, " + Math.round(d.Net_Overseas_Migration_Thousands * 10) + ")";
